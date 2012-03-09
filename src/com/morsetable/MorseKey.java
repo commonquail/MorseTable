5349d5edc8203fbc058a12cb0aeb12c6294cc657
Mikkel Kjeldsen <commonquail@gmail.com>
2012-03-09 23:02:21 +0100

    Morse code now generated as per the graphical hierarchy (was entered manually).
    Pros:
    - One dit/dah is every dit/dah.
    - Two strings.xml entries instead of 30.
    - No typos in codes.
    
    Con:
    - O(log n) function calls in place of O(1).

--- src/com/morsetable/MorseKey.java
+++ src/com/morsetable/MorseKey.java
@@ -4,35 +4,51 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MorseKey extends Button {
 
-	public MorseKey(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		// TODO Auto-generated constructor stub
-		initMorseKey(attrs);
-	}
-	
-	private void initMorseKey(AttributeSet attrs) {
-		TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.MorseKey);
-		final int N = a.getIndexCount();
-		for (int i = 0; i < N; i++)
-		{
-			int attr = a.getIndex(i);
-			switch (attr)
-			{
-			case R.styleable.MorseKey_code:
-				code = a.getString(attr);
-				break;
-			}
-		}
-		a.recycle();
-	}
+    public MorseKey(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initMorseKey(attrs);
+    }
+    
+    private void initMorseKey(AttributeSet attrs) {
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.MorseKey);
+        final int N = a.getIndexCount();
+        for (int i = 0; i < N; i++)
+        {
+            int attr = a.getIndex(i);
+            switch (attr)
+            {
+            case R.styleable.MorseKey_code:
+                code = a.getString(attr);
+                break;
+            case R.styleable.MorseKey_parentKey:
+            	// Calling this.getParent() here is too soon; the view 
+            	// hierarchy hasn't been created yet. Make it a lazy
+            	// assignment instead.
+            	parentKey = a.getResourceId(attr, -1);
+                break;
+            }
+        }
+        a.recycle();
+    }
 
-	public String code()
-	{
-		return code;
-	}
-	
-	private String code;
+    public String code()
+    {
+        if (parent == null) {
+        	parent = (MorseKey)((RelativeLayout)this.getParent().getParent()).findViewById(parentKey);
+        }
+        if (parent == this) {
+        	return code;
+        }
+        return parent.code() + code;
+    }
+    
+    private int parentKey;
+    
+    private MorseKey parent;
+    
+    private String code;
 }
